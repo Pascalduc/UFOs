df07b9421202b6d4cb829754f4bed00e9a10077c
Pascalduc <78885790+Pascalduc@users.noreply.github.com>
2021-05-05 13:05:10 -0400

    table

--- static/js/app.js
+++ static/js/app.js
@@ -1,8 +1,10 @@
 // import the data from data.js
 const tableData = data;
 
-// Reference the html table using D3 (tells JS to look for <tbody> tag in the html)
+// Reference the html table using D3 (data driver document)
+// tells JS to look for <tbody> tag in the html
 var tbody = d3.select("tbody");
+
 function buildTable(data) {
     // First, clear out any existing data
     tbody.html("");
@@ -22,3 +24,28 @@ function buildTable(data) {
       );
     });
   }
+
+  function handleClick() {
+    // Grab the datetime value from the filter
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+  
+     // Check to see if a date was entered and filter the
+    // data using that date.
+    if (date) {
+      // Apply `filter` to the table data to only keep the
+      // rows where the `datetime` value matches the filter value
+      filteredData = filteredData.filter(row => row.datetime === date);
+    };
+  
+     // Rebuild the table using the filtered data
+    // @NOTE: If no date was entered, then filteredData will
+    // just be the original tableData.
+    buildTable(filteredData);
+  };
+
+  // listen to click on webpage
+  d3.selectAll("#filter-btn").on("click", handleClick);
+
+  // call the table when the page opens
+  buildTable(tableData);
